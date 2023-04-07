b5278d94fe0b519c7c0aaeb4ff7bad6776ae9071
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-04-07 17:27:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,87 +5,44 @@
 
 
 double pown(double value, uint16_t n) {
-  double result =1.0;
-  for (int i=1, i<=power;i++)
+  double result =value;
+  for (int i=1, i<=n; i++)
   {
-    result = result*x;
+    result *= value;
   }
-  return(result);
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-  if(n < 0) 
-    return 0; 
-  if (n == 0) 
-    return 1; 
-  else 
-    return n * fact(n - 1); 
-  cout<<fact<<endl;
+  if (n == 0 || n == 1){
+    return 1;
+  }else{
+    return n * fact(n - 1);
+  }
 }
 
 double calcItem(double x, uint16_t n) {
-  int s;
-  double d=1;
-  if(n<1000000){
-    return 1;
-  }
-  else{
-    for(i=0;i<n;i++){
-      s+=1/pow(n,x);
-    cout << s <<endl;}
-  }
-  return 0;
+   return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
  
-    double result =1.0;
-    for (int i=1; i<=power;i++)
-    {
-      result += result*x/i;
-    }
-    return(result);
+    double result = 1;
+    for (uint16_t i = 1; i <= count; i++)
+        result += calcItem(x, i);
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-
-  int i=0;
-  int s=0;
-  int q = 1;
-  long int f=1;
-  int m=1;
-  double d=x;
-  double sinx1=0;
-  double sinx2=0;
-
-
-  while (i<n){
-    sinx1=sinx1+q*(d/f);
-    cout << sinx1 <<endl;
-    d=d*x*x;
-    f= f*(m+1)*(m+2);
-    m=m+2;
-    q = -q;
-    i++;}
-}
+  double result = 0;
+  for (uint64_t i = 2; i <= count; i++)
+    result += pown(-1, i-1) * calcItem(x, 2 * i - 1);
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-  int i=0;
-  int q = 1;
-  long int f=1;
-  int m=0;
-  double d=x;
-  double cosn=0;
-  
-
-  while (i<n){
-    cosn=1-q*cosn*(d/f);
-    cout << cosn <<endl;
-    
-    d=d*x*x;
-    f= f*(m+1)*(m+2);
-    m=m+2;
-    q = -q;
-    i++;}
+  double result = 0;
+  for (uint64_t i = 2; i <= count; i++)
+    result += pown(-1, i-1) * calcItem(x, 2 * i - 2);
+  return result;
 }
