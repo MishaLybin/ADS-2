8f5882a7a202a4c8cd0b6feac2b05070fe77fce0
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-04-07 19:02:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,11 +18,10 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-  if (n == 0) {
-    return 1;
-  } else {
-    return n * fact(n - 1);
-  }
+  uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i)
+        result *= i;
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
@@ -37,15 +36,15 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-  double result = 0.0;
-  for (uint64_t i = 0; i <= count; i++)
+  double result = 0;
+  for (uint64_t i = 1; i <= count; i++)
     result += pown(-1, i-1) * calcItem(x, 2 * i - 1);
   return result;
 }
 
 double cosn(double x, uint16_t count) {
   double result = 1.0;
-  for (uint64_t i = 0; i <= count; i++)
+  for (uint64_t i = 2; i <= count; i++)
     result += pown(-1, i-1) * calcItem(x, 2 * i - 2);
   return result;
 }
